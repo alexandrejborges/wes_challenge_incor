f4e2b21bef0d0292cc406ed82a456690261867a6
Alexandre J Borges Araujo <alexandrejunio@usp.br>
2025-05-29 17:20:07 -0300

    Update setup_project.sh

--- setup_project.sh
+++ setup_project.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+
 
 # =============================================================
 # Script Name: setup_project.sh
@@ -7,11 +8,33 @@ set -e
 # Description:
 #   Sets up the WES project structure and Conda environments.
 #   Creates required folders and environments for the pipeline.
+#   Download the necessary scripts
 # =============================================================
 
-# Creating directories
-echo "Creating project directory structure..."
+
+
+# Download script files
+SCRIPT_REPO_URL="https://raw.githubusercontent.com/alexandrejborges/wes_challenge_incor/main/scripts"
+SCRIPT_NAMES=(
+    "contamination_verifybamid.sh"
+    "convert_cram_to_bam.sh"
+    "coverage_mosdepth.sh"
+    "coverage_summary_and_histogram.R"
+    "download_all.sh"
+    "run_pipeline.sh"
+    "sex_inference.R"
+)
+echo "Downloading scripts to ${PROJECT_DIR}/scripts..."
+for script in "${SCRIPT_NAMES[@]}"; do
+    curl -fsSL "${SCRIPT_REPO_URL}/${script}" -o "${PROJECT_DIR}/scripts/${script}"
+    chmod +x "${PROJECT_DIR}/scripts/${script}"
+    echo " → Downloaded: ${script}"
+done
+echo "All scripts downloaded."
+
+# Creat directories
 PROJECT_DIR="wes_challenge_incor"
+echo "Creating project directory structure..."
 mkdir -p "${PROJECT_DIR}"/{data,logs,results,scripts}
 echo "Directory structure created under ${PROJECT_DIR}/"
 tree -L 2 "${PROJECT_DIR}/"
