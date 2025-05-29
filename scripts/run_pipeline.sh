b94b49da745939d68fb7e4d7aa0efa77136c11ad
Alexandre J Borges Araujo <alexandrejunio@usp.br>
2025-05-29 16:36:57 -0300

    Update run_pipeline.sh
    Fixed the first step to run coverage_mosdepth.sh correctly and added visual checks to each step.

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -18,33 +18,42 @@ mkdir -p logs
 echo "========== [1] Running Mosdepth =========="
 for CRAM in data/*.cram; do
   SAMPLE=$(basename "$CRAM" .cram)
-  echo "→ Running Mosdepth for $SAMPLE"
-  bash scripts/coverage_mosdepth.sh "$SAMPLE" | tee "logs/${SAMPLE}_mosdepth.log"
+  echo "[$(date)]→ Running Mosdepth for $SAMPLE"
+  bash scripts/coverage_mosdepth.sh "$CRAM" "$BED" "$REF" \
+    |& tee "logs/${SAMPLE}_mosdepth.log"
+  echo "[$(date)] ✔ Mosdepth Finished: $SAMPLE"
+  echo "---------------------------------------------------"
 done
 
 echo "========== [2] Exploratory Analysis + Histogram (R) =========="
 for BED_GZ in results/*.regions.bed.gz; do
   SAMPLE=$(basename "$BED_GZ" .regions.bed.gz)
-  echo "→ Exploratory coverage analysis for $SAMPLE"
+  echo "[$(date)] → Exploratory coverage analysis for $SAMPLE"
   Rscript scripts/coverage_summary_and_histogram.R "$BED_GZ" > "logs/${SAMPLE}_exploratory_analysis.log" 2>&1
+  echo "[$(date)] ✔ Coverage Analysis Finished: $SAMPLE"
+  echo "---------------------------------------------------"
 done
 
 echo "========== [3] Genetic Sex Inference (R) =========="
 for SUMMARY in results/*.mosdepth.summary.txt; do
   SAMPLE=$(basename "$SUMMARY" .mosdepth.summary.txt)
-  echo "→ Inferring genetic sex for $SAMPLE"
+  echo "[$(date)] → Inferring genetic sex for $SAMPLE"
   Rscript scripts/sex_inference.R "$SAMPLE" > "logs/${SAMPLE}_chrXY_coverage.log" 2>&1
+  echo "[$(date)] ✔ Sex Inference Finished: $SAMPLE"
+  echo "---------------------------------------------------"
 done
 
 echo "========== [4] Converting CRAM to BAM =========="
 bash scripts/convert_cram_to_bam.sh | tee logs/step4_convert_cram_to_bam.log
+echo "[$(date)] ✔ Convertion Finished: $SAMPLE"
+echo "---------------------------------------------------"
 
 echo "========== [5] Estimating Contamination with verifyBamID =========="
 
 # Handle Conda env switching with fallback
 source ~/miniconda3/etc/profile.d/conda.sh
 
-echo "→ Activating verifybamid_env..."
+echo "[$(date)] → Activating verifybamid_env..."
 set +u
 conda deactivate || true
 set -u
@@ -52,12 +61,14 @@ conda activate verifybamid_env
 
 bash scripts/contamination_verifybamid.sh | tee logs/step5_verifybamid.log
 
-echo "→ Re-activating wes_qc_env..."
+echo "[$(date)] → Re-activating wes_qc_env..."
 set +u
 conda deactivate || true
 set -u
 conda activate wes_qc_env
-
+echo "[$(date)] ✔ Estimating Contamination Finished: $SAMPLE"
+echo "---------------------------------------------------"
+  
 echo "========== Pipeline completed successfully =========="
 
 if grep -q "Error" logs/*.log; then
